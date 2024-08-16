5013b799c6a4e89dc1890551dad80bd7956ecf41
María José Arancibia Suazo <mjarancibiasuazo@gmail.com>
2024-08-16 16:37:13 -0400

    modificación del crud, icono y fecha

--- src/components/CriticityType/CriticityType.tsx
+++ src/components/CriticityType/CriticityType.tsx
@@ -3,8 +3,6 @@ import axios, { AxiosResponse } from "axios";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { showAlert } from '../functions';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import EncabezadoTabla from "../EncabezadoTabla/EncabezadoTabla";
 import * as bootstrap from 'bootstrap';
@@ -20,11 +18,10 @@ interface CriticityType {
 
 const CriticityType: React.FC = () => {
   const URL = "https://asymetricsbackend.uk.r.appspot.com/criticity_type/";
-  const [criticitytype, setCriticityType] = useState<CriticityType[]>([]);
+  const [criticity, setCriticityType] = useState<CriticityType[]>([]);
   const [id, setId] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
-  const [createDate, setCreateDate] = useState<string>("");
   const [title, setTitle] = useState<string>("");
   const modalRef = useRef<HTMLDivElement | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -50,17 +47,16 @@ const CriticityType: React.FC = () => {
     }
   };
 
-  const openModal = (op: string, criticitytype?: CriticityType) => {
-    if (criticitytype) {
-      setId(criticitytype.id);
-      setName(criticitytype.name);
-      setDescription(criticitytype.description);
-      setCreateDate(criticitytype.createDate);
+  
+  const openModal = (op: string, criticity?: CriticityType) => {
+    if (criticity) {
+      setId(criticity.id);
+      setName(criticity.name);
+      setDescription(criticity.description);
     } else {
       setId("");
       setName("");
       setDescription("");
-      setCreateDate("");
     }
     setTitle(op === "1" ? "Registrar Criticidad" : "Editar Criticidad");
 
@@ -79,53 +75,49 @@ const CriticityType: React.FC = () => {
 
   const validar = () => {
     if (name.trim() === "") {
-      showAlert("Escribe el nombre de la criticidad", "warning", "nombre");
+      showAlert("Escribe el tipo de criticidad", "warning", "tipo de criticidad");
       return;
     }
     if (description.trim() === "") {
       showAlert("Escribe la descripción", "warning", "descripción");
       return;
     }
-    if (createDate.trim() === "") {
-      showAlert("Escribe la fecha", "warning", "fecha");
-      return;
-    }
-  
-    // Asegúrate de que 'createDate' esté correctamente configurado en 'parametros'
-    const parametros = { id, name: name.trim(), description: description.trim(), createDate: createDate.trim() };
+    
+    const parametros = { id, name: name.trim(), description: description.trim() };
     const metodo = id ? "PUT" : "POST";
     enviarSolicitud(metodo, parametros);
   };
-  
 
   const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
     try {
-      const url = method === "PUT" && id ? `${URL}` : URL;
-      const response = await axios({
-        method,
-        url,
-        data,
-        headers: { "Content-Type": "application/json" },
-      });
+        // Si es una petición PUT, se añade el ID al final de la URL
+        const url = method === "PUT" && id ? `${URL}${id}` : URL;
+        const response = await axios({
+            method,
+            url,
+            data,
+            headers: { "Content-Type": "application/json" },
+        });
 
-      const { tipo, msj } = response.data;
-      showAlert(msj, tipo);
-      getUsers();
-      if (tipo === "success") {
-        // Cierra el modal después de un segundo para permitir la actualización
-        setTimeout(() => {
-          const closeModalButton = document.getElementById("btnCerrar");
-          if (closeModalButton) {
-            closeModalButton.click();
-          }
-          getUsers(); // Actualiza la lista de usuarios
-        }, 500);
-      }
+        const { tipo, msj } = response.data;
+        showAlert(msj, tipo);
+        getUsers();
+        if (tipo === "success") {
+            // Cierra el modal después de un segundo para permitir la actualización
+            setTimeout(() => {
+                const closeModalButton = document.getElementById("btnCerrar");
+                if (closeModalButton) {
+                    closeModalButton.click();
+                }
+                getUsers(); 
+            }, 500);
+        }
     } catch (error) {
-      showAlert("Error al enviar la solicitud", "error");
-      console.error(error);
+        showAlert("Error al enviar la solicitud", "error");
+        console.error(error);
     }
-  };
+};
+
 
   const deleteUser = async (id: string) => {
     try {
@@ -134,10 +126,10 @@ const CriticityType: React.FC = () => {
           'Content-Type': 'application/json'
         }
       });
-      showAlert("Usuario eliminado correctamente", "success");
+      showAlert("Criticidad eliminado correctamente", "success");
       getUsers(); 
     } catch (error) {
-      showAlert("Error al eliminar el usuario", "error");
+      showAlert("Error al eliminar la criticidad", "error");
       console.error(error);
     }
   };
@@ -154,13 +146,19 @@ const CriticityType: React.FC = () => {
 		</Tooltip>
 	  );
 
+    // Función para formatear la fecha y eliminar la hora
+    const formatDate = (dateString: string) => {
+    // Solo toma la parte de la fecha (YYYY-MM-DD)
+    return dateString.split('T')[0];
+  };
+
   return (
     <div className="App">
       <div className="container-fluid">
         <div className="row mt-3">
           <div className="col-12">
             <div className="tabla-contenedor">
-              <EncabezadoTabla title='Criticidad' onClick={() => openModal("1")} />
+              <EncabezadoTabla title='Tipo de Criticidad' onClick={() => openModal("1")} />
             </div>
             <div className="table-responsive">
               <table className="table table-bordered">
@@ -168,24 +166,24 @@ const CriticityType: React.FC = () => {
                 style={{ background: 'linear-gradient(90deg, #009FE3 0%, #00CFFF 100%)', 
                 color: '#fff' }}>
                   <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
+                    <th>N°</th>
+                    <th>Tipo</th>
                     <th>Descripción </th>
                     <th>Fecha</th>
                     <th>Acciones</th>
                   </tr>
                 </thead>
                 <tbody className="table-group-divider">
-                  {criticitytype.map((criticitytype, i) => (
-                    <tr key={criticitytype.id} className="text-center">
+                  {criticity.map((crit, i) => (
+                    <tr key={crit.id} className="text-center">
                       <td>{i + 1}</td>
-                      <td>{criticitytype.name}</td>
-                      <td>{criticitytype.description}</td>
-                      <td>{criticitytype.createDate}</td>
+                      <td>{crit.name}</td>
+                      <td>{crit.description}</td>
+                      <td>{formatDate(crit.createDate)}</td>
                       <td className="text-center">
                         <OverlayTrigger placement="top" overlay={renderEditTooltip({})}>
                         <button
-                        onClick={() => openModal("2", criticitytype)}
+                        onClick={() => openModal("2", crit)}
                         className="btn btn-custom-editar m-2"
                         data-bs-toggle="modal"
                         data-bs-target="#modalUsers"
@@ -204,12 +202,11 @@ const CriticityType: React.FC = () => {
                             cancelButtonText: "Cancelar",
                           }).then((result) => {
                             if (result.isConfirmed) {
-                              deleteUser(criticitytype.id);
+                              deleteUser(crit.id);
                             }
                           });
-                        }}
-                        >
-                          <FontAwesomeIcon icon={faCircleXmark} />
+                        }}>
+                          <i className="fa-solid fa-circle-xmark"></i>
                         </button>
                       </OverlayTrigger>
                       </td>
@@ -220,19 +217,11 @@ const CriticityType: React.FC = () => {
             </div>
           </div>
         </div>
-        <div
-          className="modal fade"
-          id="modalUsers"
-          ref={modalRef}
-        data-bs-backdrop="true"
-        data-bs-keyboard="true"
-          aria-labelledby="staticBackdropLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
+        <div className="modal fade" id="modalUsers" tabIndex={-1} aria-hidden="true" ref={modalRef}>
+          <div className="modal-dialog modal-dialog-top modal-md">
             <div className="modal-content">
-              <div className="modal-header text-white">
-                <label className="h5">{title}</label>
+              <div className="modal-header">
+                <h5 className="modal-title w-100">{title}</h5>
                 <button
                   type="button"
                   className="btn-close"
@@ -244,13 +233,13 @@ const CriticityType: React.FC = () => {
                 <input type="hidden" id="id" />
                 <div className="input-group mb-3">
                   <span className="input-group-text">
-                    <i className="fa-solid fa-user"></i>
+                  <i className="fa-solid fa-bolt"></i>
                   </span>
                   <input
                     type="text"
                     id="nombre"
                     className="form-control"
-                    placeholder="Nombre"
+                    placeholder="Tipo de Criticidad"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                   />
@@ -269,17 +258,6 @@ const CriticityType: React.FC = () => {
                   />
                 </div>
                 <div className="input-group mb-3">
-                  <span className="input-group-text">
-                    <i className="fa-solid fa-calendar-days"></i>
-                  </span>
-                  <input
-                    type="text"
-                    id="createDate"
-                    className="form-control"
-                    placeholder="Fecha de Creación"
-                    value={createDate}
-                    onChange={(e) => setCreateDate(e.target.value)}
-                  />
                 </div>
               </div>
               <div className="modal-footer">
@@ -304,6 +282,7 @@ const CriticityType: React.FC = () => {
         </div>
       </div>
     </div>
+  
   );
 };
 
