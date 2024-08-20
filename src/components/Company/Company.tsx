8c53fa0c866d4ce5e3b1946b30a0d141d466f8a7
Jhidalgo - Asymetrics <jhidalgo@asymetrics.cl>
2024-08-20 14:22:47 -0400

    Actualizacion Company

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -3,8 +3,6 @@ import axios, { AxiosResponse } from "axios";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { showAlert } from '../functions';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import EncabezadoTabla from "../EncabezadoTabla/EncabezadoTabla";
 import * as bootstrap from 'bootstrap';
@@ -15,24 +13,21 @@ interface Company {
   id: string;
   name: string;
   description: string;
-  createDate: string;
-  // createDate: Date;
 }
 
-const Companies: React.FC = () => { 
-  const URL = "https://asymetricsbackend.uk.r.appspot.com/company/";
-  const [companies, setCompanies] = useState<Company[]>([]); 
+const Company: React.FC = () => {
+
+  const URL = "https://asymetricsbackend.uk.r.appspot.com/company";
+  const [company, setCompany] = useState<Company[]>([]);
   const [id, setId] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
-  const [createDate, setCreateDate] = useState<string>(""); 
-  // const [createDate, setCreateDate] = useState<Date | null>(null);
   const [title, setTitle] = useState<string>("");
   const modalRef = useRef<HTMLDivElement | null>(null);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
-    fetchCompanies();
+    getUsers();
     if (modalRef.current) {
       modalRef.current.addEventListener('hidden.bs.modal', handleModalHidden);
     }
@@ -43,38 +38,27 @@ const Companies: React.FC = () => {
     };
   }, []);
 
-  const fetchCompanies = async () => {
+  const getUsers = async () => {
     try {
       const response: AxiosResponse<Company[]> = await axios.get(URL);
-      const formattedCompanies = response.data.map((company) => ({
-        ...company,
-        createDate: new Date(company.createDate).toLocaleString(),
-      }));
-      setCompanies(formattedCompanies);
+      setCompany(response.data);
     } catch (error) {
-      showAlert("Error al obtener los datos de Company", "error");
+      showAlert("Error al obtener Compañias", "error");
     }
   };
 
-  const openModal = (op: string, company?: Company) => { 
+  
+  const openModal = (op: string, company?: Company) => {
     if (company) {
       setId(company.id);
       setName(company.name);
       setDescription(company.description);
-      // setCreateDate(new Date(profile.createDate));
-      setCreateDate(company.createDate);
     } else {
       setId("");
       setName("");
       setDescription("");
-      // setCreateDate(null);
-      setCreateDate("");
     }
     setTitle(op === "1" ? "Registrar Compañia" : "Editar Compañia");
-
-    setTimeout(() => {
-      document.getElementById("nombre")?.focus();
-    }, 500);
 
     if (modalRef.current) {
       const modal = new bootstrap.Modal(modalRef.current);
@@ -85,74 +69,65 @@ const Companies: React.FC = () => {
 
   const handleModalHidden = () => {
     setIsModalOpen(false);
-    const modals = document.querySelectorAll(".modal-backdrop");
-    modals.forEach((modal) => modal.parentNode?.removeChild(modal));
+    const modals = document.querySelectorAll('.modal-backdrop');
+    modals.forEach(modal => modal.parentNode?.removeChild(modal));
   };
 
   const validar = () => {
     if (name.trim() === "") {
-      showAlert("Escribe el nombre", "warning", "nombre");
+      showAlert("Escribe el nombre", "warning", "nombre de la compañia");
       return;
     }
     if (description.trim() === "") {
-      showAlert("Escribe la descripción", "warning", "description");
+      showAlert("Escribe la descripción", "warning", "descripción");
       return;
     }
-    if (createDate.trim() === "") {
-      showAlert("Escribe la fecha de creación", "warning", "createDate");
-      return;
-    }
-
-    const parametros = {
-      id,
-      name: name.trim(),
-      description: description.trim(),
-      // createDate: createDate || new Date(),
-      createDate: createDate.trim(),
-    };
+    
+    const parametros = { id, name: name.trim(), description: description.trim() };
     const metodo = id ? "PUT" : "POST";
     enviarSolicitud(metodo, parametros);
   };
 
   const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
     try {
-      const url = method === "PUT" && id ? `${URL}${id}` : URL;
-      const response = await axios({
-        method,
-        url,
-        data,
-        headers: { "Content-Type": "application/json" },
-      });
+        const url = method === "PUT" && id ? `${URL}${id}` : URL;
+        const response = await axios({
+            method,
+            url,
+            data,
+            headers: { "Content-Type": "application/json" },
+        });
 
-      const { tipo, msj } = response.data;
-      showAlert(msj, tipo);
-      fetchCompanies();
-      if (tipo === "success") {
-        setTimeout(() => {
-          const closeModalButton = document.getElementById("btnCerrar");
-          if (closeModalButton) {
-            closeModalButton.click();
-          }
-          fetchCompanies();
-        }, 500);
-      }
+        const { tipo, msj } = response.data;
+        showAlert(msj, tipo);
+        getUsers();
+        if (tipo === "success") {
+            setTimeout(() => {
+                const closeModalButton = document.getElementById("btnCerrar");
+                if (closeModalButton) {
+                    closeModalButton.click();
+                }
+                getUsers(); 
+            }, 500);
+        }
     } catch (error) {
-      showAlert("Error al enviar la solicitud", "error");
-      console.error(error);
+        showAlert("Error al enviar la solicitud", "error");
+        console.error(error);
     }
-  };
+};
 
-  const deleteCompany = async (id: string) => {
+
+  const deleteUser = async (id: string) => {
     try {
       await axios.delete(`${URL}${id}`, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      showAlert("Perfil eliminado correctamente", "success");
-      fetchCompanies();
+      showAlert("Proceso eliminado correctamente", "success");
+      getUsers(); 
     } catch (error) {
-      showAlert("Error al eliminar el perfil", "error");
+      showAlert("Error al eliminar el proceso", "error");
       console.error(error);
     }
   };
@@ -169,47 +144,45 @@ const Companies: React.FC = () => {
 		</Tooltip>
 	  );
 
+
   return (
     <div className="App">
       <div className="container-fluid">
         <div className="row mt-3">
           <div className="col-12">
             <div className="tabla-contenedor">
-              <EncabezadoTabla title='Company' onClick={() => openModal("1")} />
+              <EncabezadoTabla title='Compañias' onClick={() => openModal("1")} />
             </div>
             <div className="table-responsive">
               <table className="table table-bordered">
-                <thead className="text-center"
-                  style={{ background: 'linear-gradient(90deg, #009FE3 0%, #00CFFF 100%)', color: '#fff' }}>
+                <thead className="text-center" 
+                style={{ background: 'linear-gradient(90deg, #009FE3 0%, #00CFFF 100%)', 
+                color: '#fff' }}>
                   <tr>
-                    <th>ID</th>
+                    <th>N°</th>
                     <th>Nombre</th>
-                    <th>Descripción</th>
-                    <th>Fecha de Creación</th>
+                    <th>Descripción </th>
                     <th>Acciones</th>
                   </tr>
                 </thead>
                 <tbody className="table-group-divider">
-                  {companies.map((company, i) => (
-                    <tr key={company.id} className="text-center">
+                  {company.map((comp, i) => (
+                    <tr key={comp.id} className="text-center">
                       <td>{i + 1}</td>
-                      <td>{company.name}</td>
-                      <td>{company.description}</td>
-                      {/* <td>{new Date(company.createDate).toLocaleString()}</td> */}
-                      {/* <td>{company.createDate}</td> */}
-                      <td>29/7/2024</td>
+                      <td>{comp.name}</td>
+                      <td>{comp.description}</td>
                       <td className="text-center">
                         <OverlayTrigger placement="top" overlay={renderEditTooltip({})}>
                         <button
-                          onClick={() => openModal("2", company)}
-                          className="btn btn-custom-editar m-2"
-                          data-bs-toggle="modal"
-                          data-bs-target="#modalCompany"
-                        >
-                          <i className="fa-solid fa-edit"></i>
-                        </button>
-                        </OverlayTrigger>
-                        <OverlayTrigger placement="top" overlay={renderDeleteTooltip({})}>
+                        onClick={() => openModal("2", comp)}
+                        className="btn btn-custom-editar m-2"
+                        data-bs-toggle="modal"
+                        data-bs-target="#modalUsers"
+                      >
+                        <i className="fa-solid fa-edit"></i>
+                      </button>
+                      </OverlayTrigger>
+                      <OverlayTrigger placement="top" overlay={renderDeleteTooltip({})}>
                         <button className="btn btn-custom-danger" onClick={() => {
                           MySwal.fire({
                             title: "¿Estás seguro?",
@@ -220,13 +193,13 @@ const Companies: React.FC = () => {
                             cancelButtonText: "Cancelar",
                           }).then((result) => {
                             if (result.isConfirmed) {
-                              deleteCompany(company.id);
+                              deleteUser(comp.id);
                             }
                           });
                         }}>
-                          <FontAwesomeIcon icon={faCircleXmark} />
+                          <i className="fa-solid fa-circle-xmark"></i>
                         </button>
-                        </OverlayTrigger>
+                      </OverlayTrigger>
                       </td>
                     </tr>
                   ))}
@@ -235,18 +208,11 @@ const Companies: React.FC = () => {
             </div>
           </div>
         </div>
-        <div
-          className="modal fade"
-          id="modalCompany"
-          data-bs-backdrop="true"
-          data-bs-keyboard="true"
-          aria-labelledby="staticBackdropLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
+        <div className="modal fade" id="modalUsers" tabIndex={-1} aria-hidden="true" ref={modalRef}>
+          <div className="modal-dialog modal-dialog-top modal-md">
             <div className="modal-content">
-              <div className="modal-header text-white">
-                <label className="h5">{title}</label>
+              <div className="modal-header">
+                <h5 className="modal-title w-100">{title}</h5>
                 <button
                   type="button"
                   className="btn-close"
@@ -258,24 +224,24 @@ const Companies: React.FC = () => {
                 <input type="hidden" id="id" />
                 <div className="input-group mb-3">
                   <span className="input-group-text">
-                    <i className="fa-solid fa-user"></i>
+                  <i className="fa-solid fa-list-check"></i>
                   </span>
                   <input
                     type="text"
                     id="nombre"
                     className="form-control"
-                    placeholder="Nombre"
+                    placeholder="Nombre de la Compañia"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                   />
                 </div>
                 <div className="input-group mb-3">
                   <span className="input-group-text">
-                    <i className="fa-regular fa-envelope"></i>
+                    <i className="fa-regular fa-solid fa-file-alt"></i>
                   </span>
                   <input
                     type="text"
-                    id="description"
+                    id="descripcion"
                     className="form-control"
                     placeholder="Descripción"
                     value={description}
@@ -283,48 +249,32 @@ const Companies: React.FC = () => {
                   />
                 </div>
                 <div className="input-group mb-3">
-                  <span className="input-group-text">
-                    <i className="fa-solid fa-calendar"></i>
-                  </span>
-                  <input
-                    type="text"
-                    id="createDate"
-                    className="form-control"
-                    placeholder="Fecha de creación"
-                    value={createDate}
-                    onChange={(e) => setCreateDate(e.target.value)}
-                  />
-                  {/* <input
-                    type="datetime-local"
-                    id="createDate"
-                    className="form-control"
-                    value={createDate ? createDate.toISOString().substring(0, 16) : ""}
-                    onChange={(e) => setCreateDate(e.target.value ? new Date(e.target.value) : null)}
-                  /> */}
-                </div>
-                <div className="input-group mb-3">
-                  <span className="input-group-text">
-                    <i className="fa-solid fa-tasks"></i>
-                  </span>
-                </div>
-                <div className="d-grid col-6 mx-auto">
-                  <button onClick={validar} className="btn btn-success">
-                    <i className="fa-solid fa-floppy-disk m-2"></i>Guardar
-                  </button>
                 </div>
               </div>
               <div className="modal-footer">
-                <button type="button"
-                  className="btn btn-secondary m-2"
+                <button
+                  type="button"
+                  className="btn btn-secondary"
                   data-bs-dismiss="modal"
-                  id="btnCerrar">Cerrar</button>
+                  id="btnCerrar"
+                >
+                  Cerrar
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={validar}
+                >
+                  Guardar
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  
   );
 };
 
-export default Companies;
+export default Company;
