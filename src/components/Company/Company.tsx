e6982cdadf3edc88ef3cb8aaba73c0e9908491ac
julioHidalgo <jhidalgo@asymetrics.cl>
2024-09-06 16:51:17 -0400

    modificacion compañia

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,3 +1,293 @@
+// import React, { useEffect, useState, useRef } from "react";
+// import axios, { AxiosResponse } from "axios";
+// import Swal from "sweetalert2";
+// import withReactContent from "sweetalert2-react-content";
+// import { showAlert } from '../functions';
+// import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+// import EncabezadoTabla from "../EncabezadoTabla/EncabezadoTabla";
+// import * as bootstrap from 'bootstrap';
+
+// const MySwal = withReactContent(Swal);
+
+// interface Company {
+//   id: number;                  
+//   name: string;
+//   description: string;
+//   createDate: string;  
+//   updateDate: string; 
+// }
+
+
+// const Company: React.FC = () => {
+
+//   const baseURL = import.meta.env.VITE_API_URL;
+//   const [company, setCompany] = useState<Company[]>([]);
+//   const [id, setId] = useState<number | null>(null);
+//   const [name, setName] = useState<string>("");
+//   const [description, setDescription] = useState<string>("");
+//   const [title, setTitle] = useState<string>("");
+//   const modalRef = useRef<HTMLDivElement | null>(null);
+//   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+//   useEffect(() => {
+//     getCompany();
+//     if (modalRef.current) {
+//       modalRef.current.addEventListener('hidden.bs.modal', handleModalHidden);
+//     }
+//     return () => {
+//       if (modalRef.current) {
+//         modalRef.current.removeEventListener('hidden.bs.modal', handleModalHidden);
+//       }
+//     };
+//   }, []);
+
+//   const getCompany = async () => {
+//     try {
+//       const response: AxiosResponse<Company[]> = await axios.get(`${baseURL}/company/`);
+//       setCompany(response.data);
+//     } catch (error) {
+//       showAlert("Error al obtener Compañias", "error");
+//     }
+//   };
+
+
+//   const openModal = (op: string, company?: Company) => {
+//     if (company) {
+//       setId(0);
+//       setName(company.name);
+//       setDescription(company.description);
+//     } else {
+//       setId(0);
+//       setName("");
+//       setDescription("");
+//     }
+//     setTitle(op === "1" ? "Registrar Compania" : "Editar Compania");
+
+//     if (modalRef.current) {
+//       const modal = new bootstrap.Modal(modalRef.current);
+//       modal.show();
+//       setIsModalOpen(true);
+//     }
+//   };
+
+//   const handleModalHidden = () => {
+//     setIsModalOpen(false);
+//     const modals = document.querySelectorAll('.modal-backdrop');
+//     modals.forEach(modal => modal.parentNode?.removeChild(modal));
+//   };
+
+//   const validar = () => {
+//     if (name.trim() === "") {
+//       showAlert("Escribe el nombre", "warning", "nombre de la compañia");
+//       return;
+//     }
+//     if (description.trim() === "") {
+//       showAlert("Escribe la descripción", "warning", "descripción");
+//       return;
+//     }
+    
+//     const parametros = { id, name: name.trim(), description: description.trim() };
+//     const metodo = id ? "PUT" : "POST";
+//     enviarSolicitud(metodo, parametros);
+//   };
+
+//   const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
+//     try {
+//       const url = method === "PUT" && id ? `${baseURL}/company/${id}` : `${baseURL}/company/`;
+//       const response = await axios({
+//       method,
+//       url,
+//       data,
+//       headers: { "Content-Type": "application/json" },
+//       });
+    
+//       showAlert("Operación realizada con éxito", "success");
+//       getCompany();
+//       if (modalRef.current) {
+//       const modal = bootstrap.Modal.getInstance(modalRef.current);
+//       modal?.hide();
+//       }
+//     } catch (error) {
+//       if (axios.isAxiosError(error) && error.response) {
+//       showAlert(`Error: ${error.response.data.message || "No se pudo completar la solicitud."}`, "error");
+//       } else {
+//       showAlert("Error al realizar la solicitud", "error");
+//       }
+//     }
+//     }; 
+
+
+
+//   const deleteCompany = async (id: number) => {
+//     try {
+//       await axios.delete(`${baseURL}/company/${id}`, {
+//         headers: { "Content-Type": "application/json" },
+//       });
+//       Swal.fire("Compañia eliminado correctamente", "", "success");
+//       getCompany();
+//     } catch (error) {
+//       Swal.fire({
+//         title: "Error",
+//         text: "Error al eliminar el Compañia.",
+//         icon: "error",
+//         confirmButtonText: "OK",
+//       });
+//     }
+//   };
+
+//   const renderEditTooltip = (props: React.HTMLAttributes<HTMLDivElement>) => (
+// 		<Tooltip id="button-tooltip-edit" {...props}>
+// 		  Editar
+// 		</Tooltip>
+// 	  );
+	  
+// 	  const renderDeleteTooltip = (props: React.HTMLAttributes<HTMLDivElement>) => (
+// 		<Tooltip id="button-tooltip-delete" {...props}>
+// 		  Eliminar
+// 		</Tooltip>
+// 	  );
+
+//     const formatDate = (dateString: string) => {
+//       return dateString.split('T')[0];
+//     };
+
+
+//   return (
+//     <div className="App">
+//       <div className="container-fluid">
+//         <div className="row mt-3">
+//           <div className="col-12">
+//             <div className="tabla-contenedor">
+//               <EncabezadoTabla title='Compañías' onClick={() => openModal("1")} />
+//             </div>
+//             <div className="table-responsive">
+//               <table className="table table-bordered">
+//                 <thead className="text-center" 
+//                 style={{ background: 'linear-gradient(90deg, #009FE3 0%, #00CFFF 100%)', 
+//                 color: '#fff' }}>
+//                   <tr>
+//                     <th>N°</th>
+//                     <th>Nombre</th>
+//                     <th>Descripción</th>
+//                     <th>Fecha</th>
+//                     <th>Acciones</th>
+//                   </tr>
+//                 </thead>
+//                 <tbody className="table-group-divider">
+//                   {company.map((comp, i) => (
+//                     <tr key={comp.id} className="text-center">
+//                       <td>{i + 1}</td>
+//                       <td>{comp.name}</td>
+//                       <td>{comp.description}</td>
+//                       <td>{formatDate(comp.createDate)}</td>
+//                       <td className="text-center">
+//                         <OverlayTrigger placement="top" overlay={renderEditTooltip({})}>
+//                         <button
+//                         onClick={() => openModal("2", comp)}
+//                         className="btn btn-custom-editar m-2"
+//                         data-bs-toggle="modal"
+//                         data-bs-target="#modalUsers"
+//                       >
+//                         <i className="fa-solid fa-edit"></i>
+//                       </button>
+//                       </OverlayTrigger>
+//                       <OverlayTrigger placement="top" overlay={renderDeleteTooltip({})}>
+//                         <button className="btn btn-custom-danger" onClick={() => {
+//                           MySwal.fire({
+//                             title: "¿Estás seguro?",
+//                             text: "No podrás revertir esto",
+//                             icon: "warning",
+//                             showCancelButton: true,
+//                             confirmButtonText: "Sí, bórralo",
+//                             cancelButtonText: "Cancelar",
+//                           }).then((result) => {
+//                             if (result.isConfirmed) {
+//                               deleteCompany(comp.id);
+//                             }
+//                           });
+//                         }}>
+//                           <i className="fa-solid fa-circle-xmark"></i>
+//                         </button>
+//                       </OverlayTrigger>
+//                       </td>
+//                     </tr>
+//                   ))}
+//                 </tbody>
+//               </table>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="modal fade" id="modalUsers" tabIndex={-1} aria-hidden="true" ref={modalRef}>
+//           <div className="modal-dialog modal-dialog-top modal-md">
+//             <div className="modal-content">
+//               <div className="modal-header">
+//                 <h5 className="modal-title w-100">{title}</h5>
+//                 <button
+//                   type="button"
+//                   className="btn-close"
+//                   data-bs-dismiss="modal"
+//                   aria-label="Close"
+//                 ></button>
+//               </div>
+//               <div className="modal-body">
+//                 <input type="hidden" id="id" />
+//                 <div className="input-group mb-3">
+//                   <span className="input-group-text">
+//                   <i className="fa-solid fa-building"></i>
+//                   </span>
+//                   <input
+//                     type="text"
+//                     id="nombre"
+//                     className="form-control"
+//                     placeholder="Nombre de la Compañia"
+//                     value={name}
+//                     onChange={(e) => setName(e.target.value)}
+//                   />
+//                 </div>
+//                 <div className="input-group mb-3">
+//                   <span className="input-group-text">
+//                     <i className="fa-regular fa-solid fa-file-alt"></i>
+//                   </span>
+//                   <input
+//                     type="text"
+//                     id="descripcion"
+//                     className="form-control"
+//                     placeholder="Descripción"
+//                     value={description}
+//                     onChange={(e) => setDescription(e.target.value)}
+//                   />
+//                 </div>
+//               </div>
+//               <div className="modal-footer">
+//                 <button
+//                   type="button"
+//                   className="btn btn-secondary"
+//                   data-bs-dismiss="modal"
+//                   id="btnCerrar"
+//                 >
+//                   Cerrar
+//                 </button>
+//                 <button
+//                   type="button"
+//                   className="btn btn-primary"
+//                   onClick={validar}
+//                 >
+//                   Guardar
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+  
+//   );
+// };
+
+// export default Company;
+
+
+
 import React, { useEffect, useState, useRef } from "react";
 import axios, { AxiosResponse } from "axios";
 import Swal from "sweetalert2";
@@ -10,18 +300,16 @@ import * as bootstrap from 'bootstrap';
 const MySwal = withReactContent(Swal);
 
 interface Company {
-  id: number;                  
+  id: number;
   name: string;
   description: string;
-  createDate: string;  
-  updateDate: string; 
+  createDate: string;
 }
-
 
 const Company: React.FC = () => {
 
   const baseURL = import.meta.env.VITE_API_URL;
-  const [company, setCompany] = useState<Company[]>([]);
+  const [company, setProcess] = useState<Company[]>([]);
   const [id, setId] = useState<number | null>(null);
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
@@ -44,25 +332,24 @@ const Company: React.FC = () => {
   const getCompany = async () => {
     try {
       const response: AxiosResponse<Company[]> = await axios.get(`${baseURL}/company/`);
-      setCompany(response.data);
+      setProcess(response.data);
     } catch (error) {
-      showAlert("Error al obtener Compañias", "error");
+      showAlert("Error al obtener Compañia", "error");
     }
   };
-
 
   
   const openModal = (op: string, company?: Company) => {
     if (company) {
-      setId(null);
+      setId(company.id);
       setName(company.name);
       setDescription(company.description);
     } else {
-      setId(0);
+      setId(null);
       setName("");
       setDescription("");
     }
-    setTitle(op === "1" ? "Registrar Compañia" : "Editar Compañia");
+    setTitle(op === "1" ? "Registrar Coma" : "Editar Compañia");
 
     if (modalRef.current) {
       const modal = new bootstrap.Modal(modalRef.current);
@@ -79,7 +366,7 @@ const Company: React.FC = () => {
 
   const validar = () => {
     if (name.trim() === "") {
-      showAlert("Escribe el nombre", "warning", "nombre de la compañia");
+      showAlert("Escribe el nombre", "warning", "nombre del Compañia");
       return;
     }
     if (description.trim() === "") {
@@ -92,43 +379,42 @@ const Company: React.FC = () => {
     enviarSolicitud(metodo, parametros);
   };
 
-const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
-  try {
-    const url = method === "PUT" && id ? `${baseURL}/company/${id}` : `${baseURL}/company/`;
-    const response = await axios({
+  const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
+    try {
+      const url = method === "PUT" && id ? `${baseURL}/company/${id}` : `${baseURL}/company/`;
+      const response = await axios({
       method,
       url,
       data,
       headers: { "Content-Type": "application/json" },
-    });
-
-    showAlert("Operación realizada con éxito", "success");
-    getCompany();
-    if (modalRef.current) {
+      });
+    
+      showAlert("Operación realizada con éxito", "success");
+      getCompany();
+      if (modalRef.current) {
       const modal = bootstrap.Modal.getInstance(modalRef.current);
       modal?.hide();
-    }
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
       showAlert(`Error: ${error.response.data.message || "No se pudo completar la solicitud."}`, "error");
-    } else {
+      } else {
       showAlert("Error al realizar la solicitud", "error");
+      }
     }
-  }
-}; 
-
+    }; 
 
   const deleteCompany = async (id: number) => {
     try {
       await axios.delete(`${baseURL}/company/${id}`, {
         headers: { "Content-Type": "application/json" },
       });
-      Swal.fire("Compañia eliminada correctamente", "", "success");
+      Swal.fire("Compañia eliminado correctamente", "", "success");
       getCompany();
     } catch (error) {
       Swal.fire({
         title: "Error",
-        text: "Error al eliminar la Compañia.",
+        text: "Error al eliminar el Compañia.",
         icon: "error",
         confirmButtonText: "OK",
       });
@@ -148,9 +434,8 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
 	  );
 
     const formatDate = (dateString: string) => {
-      return dateString.split('T')[0];
-    };
-
+    return dateString.split('T')[0];
+  };
 
   return (
     <div className="App">
@@ -158,7 +443,7 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
         <div className="row mt-3">
           <div className="col-12">
             <div className="tabla-contenedor">
-              <EncabezadoTabla title='Compañías' onClick={() => openModal("1")} />
+              <EncabezadoTabla title='Procesos' onClick={() => openModal("1")} />
             </div>
             <div className="table-responsive">
               <table className="table table-bordered">
@@ -168,7 +453,7 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
                   <tr>
                     <th>N°</th>
                     <th>Nombre</th>
-                    <th>Descripción</th>
+                    <th>Descripción </th>
                     <th>Fecha</th>
                     <th>Acciones</th>
                   </tr>
@@ -186,8 +471,7 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
                         onClick={() => openModal("2", comp)}
                         className="btn btn-custom-editar m-2"
                         data-bs-toggle="modal"
-                        data-bs-target="#modalUsers"
-                      >
+                        data-bs-target="#modalUsers">
                         <i className="fa-solid fa-edit"></i>
                       </button>
                       </OverlayTrigger>
@@ -233,13 +517,13 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
                 <input type="hidden" id="id" />
                 <div className="input-group mb-3">
                   <span className="input-group-text">
-                  <i className="fa-solid fa-building"></i>
+                  <i className="fa-solid fa-list-check"></i>
                   </span>
                   <input
                     type="text"
                     id="nombre"
                     className="form-control"
-                    placeholder="Nombre de la Compañia"
+                    placeholder="Nombre del Compañia"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                   />
@@ -256,6 +540,8 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
                   />
+                </div>
+                <div className="input-group mb-3">
                 </div>
               </div>
               <div className="modal-footer">
@@ -285,3 +571,4 @@ const enviarSolicitud = async (method: "POST" | "PUT", data: any) => {
 };
 
 export default Company;
+
